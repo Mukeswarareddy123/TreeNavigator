7ac8e4a545d9096a93bf3074ea393bb774f7a820
MukeswaraReddy Palle <120121110+Mukeswarareddy123@users.noreply.github.com>
2024-07-18 15:02:15 +0530

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,152 @@
+class Node {
+    int value;
+    Node left, right;
+
+    Node(int item) {
+        value = item;
+        left = right = null;
+    }
+}
+
+class BinarySearchTree {
+    Node root;
+
+    BinarySearchTree() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.value) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.value) {
+            root.right = insertRec(root.right, key);
+        }
+        return root;
+    }
+
+    boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    boolean searchRec(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+        if (key == root.value) {
+            return true;
+        }
+        if (key < root.value) {
+            return searchRec(root.left, key);
+        } else {
+            return searchRec(root.right, key);
+        }
+    }
+
+    void delete(int key) {
+        root = deleteRec(root, key);
+    }
+
+    Node deleteRec(Node root, int key) {
+        if (root == null) {
+            return root;
+        }
+        if (key < root.value) {
+            root.left = deleteRec(root.left, key);
+        } else if (key > root.value) {
+            root.right = deleteRec(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            root.value = minValue(root.right);
+            root.right = deleteRec(root.right, root.value);
+        }
+        return root;
+    }
+
+    int minValue(Node root) {
+        int minValue = root.value;
+        while (root.left != null) {
+            minValue = root.left.value;
+            root = root.left;
+        }
+        return minValue;
+    }
+
+    void inOrder() {
+        inOrderRec(root);
+        System.out.println();
+    }
+
+    void inOrderRec(Node root) {
+        if (root != null) {
+            inOrderRec(root.left);
+            System.out.print(root.value + " ");
+            inOrderRec(root.right);
+        }
+    }
+
+    void preOrder() {
+        preOrderRec(root);
+        System.out.println();
+    }
+
+    void preOrderRec(Node root) {
+        if (root != null) {
+            System.out.print(root.value + " ");
+            preOrderRec(root.left);
+            preOrderRec(root.right);
+        }
+    }
+
+    void postOrder() {
+        postOrderRec(root);
+        System.out.println();
+    }
+
+    void postOrderRec(Node root) {
+        if (root != null) {
+            postOrderRec(root.left);
+            postOrderRec(root.right);
+            System.out.print(root.value + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.insert(50);
+        bst.insert(30);
+        bst.insert(20);
+        bst.insert(40);
+        bst.insert(70);
+        bst.insert(60);
+        bst.insert(80);
+
+        System.out.print("InOrder Traversal: ");
+        bst.inOrder();
+
+        System.out.print("PreOrder Traversal: ");
+        bst.preOrder();
+
+        System.out.print("PostOrder Traversal: ");
+        bst.postOrder();
+
+        System.out.println("Search for 40: " + bst.search(40));
+        System.out.println("Search for 90: " + bst.search(90));
+
+        bst.delete(20);
+        System.out.print("InOrder Traversal after deleting 20: ");
+        bst.inOrder();
+    }
+}
